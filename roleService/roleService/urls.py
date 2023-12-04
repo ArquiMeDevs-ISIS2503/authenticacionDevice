d03dd00e7f35b83821851f07d82730bfa698a8e4
dcuevasa <d.cuevas@uniandes.edu.co>
2023-12-04 14:10:13 -0500

    fix names

--- roleService/roleService/urls.py
+++ roleService/roleService/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', include(('checkRole.urls','checkRole'), namespace='checkRole')),
+    url('', include(('checkRole.urls','checkRole'))),
 ]
