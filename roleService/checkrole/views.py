fecda3f9af2f55380c5bc17ed097a1449242f32f
dcuevasa <d.cuevas@uniandes.edu.co>
2023-12-04 14:07:21 -0500

    asd

--- roleService/checkrole/views.py
+++ roleService/checkrole/views.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import requests
-
+import json
 from django.shortcuts import render
 
 # Create your views here.
 
 def getRole(request):
-    user = request.user
+    user = json.loads(request.body["user"])
     auth0user = user.social_auth.get(provider="auth0")
     accessToken = auth0user.extra_data['access_token']
     url = "https://isis2503-arquimedevs.us.auth0.com/userinfo"
